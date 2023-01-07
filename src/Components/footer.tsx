a5c61a7dd3758e597bd733e8d91ac8666359b919
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-07 12:02:02 -0800

    Add tooltips

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Stack } from "@mui/material";
+import { Box, Link, Stack, Tooltip } from "@mui/material";
 import {
   FileCodeIcon,
   MentionIcon,
@@ -12,33 +12,39 @@ export default class Footer extends React.Component {
       <div>
         <Box component="footer" sx={{ py: 4 }}>
           <Stack spacing={4} direction="row" justifyContent="center">
-            <Link
-              href="https://xsalazar.com"
-              color="textPrimary"
-              aria-label="Contact Me"
-              target="_blank"
-              rel="noopener"
-            >
-              <MentionIcon size="small" verticalAlign="middle" />
-            </Link>
-            <Link
-              href="https://github.com/xsalazar/wow-emoji"
-              color="textPrimary"
-              aria-label="Source Code"
-              target="_blank"
-              rel="noopener"
-            >
-              <FileCodeIcon size="small" verticalAlign="middle" />
-            </Link>
-            <Link
-              href="https://github.com/sponsors/xsalazar"
-              color="textPrimary"
-              aria-label="Support Me"
-              target="_blank"
-              rel="noopener"
-            >
-              <SponsorTiersIcon size="small" verticalAlign="middle" />
-            </Link>
+            <Tooltip title="Contact Me">
+              <Link
+                href="https://xsalazar.com"
+                color="textPrimary"
+                aria-label="Contact Me"
+                target="_blank"
+                rel="noopener"
+              >
+                <MentionIcon size="small" verticalAlign="middle" />
+              </Link>
+            </Tooltip>
+            <Tooltip title="Source Code">
+              <Link
+                href="https://github.com/xsalazar/wow-emoji"
+                color="textPrimary"
+                aria-label="Source Code"
+                target="_blank"
+                rel="noopener"
+              >
+                <FileCodeIcon size="small" verticalAlign="middle" />
+              </Link>
+            </Tooltip>
+            <Tooltip title="Support Me">
+              <Link
+                href="https://github.com/sponsors/xsalazar"
+                color="textPrimary"
+                aria-label="Support Me"
+                target="_blank"
+                rel="noopener"
+              >
+                <SponsorTiersIcon size="small" verticalAlign="middle" />
+              </Link>
+            </Tooltip>
           </Stack>
         </Box>
       </div>
